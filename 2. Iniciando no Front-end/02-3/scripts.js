9084b0f2822c15217da1759968dc50576090c585
Lucas <abreu.lucass@outlook.com>
2020-01-29 10:15:05 -0300

    finished 3rd challenge
    added maximize button, it works!
    also added the iframes webpages and changed the css related to the modal margins

--- 2. Iniciando no Front-end/02-3/scripts.js
+++ 2. Iniciando no Front-end/02-3/scripts.js
@@ -1,15 +1,37 @@
 const modals = document.querySelectorAll('.card')
+const modalOverlay = document.querySelector('.modal-overlay')
 
 for (let modal of modals)
-{    
+{   
     modal.addEventListener("click", function()
     {
-        document.querySelector('.modal-overlay').classList.add('active')
+        let pageId = modal.getAttribute('id')
+
+        document.querySelector('iframe').setAttribute('src',`https://rocketseat.com.br/${pageId}`);
+
+        modalOverlay.classList.add('active')
     })
 }
 
+// Fechar o Modal
 
-document.querySelector('.close-modal').addEventListener("click", function()
+document.querySelector('.close-modal').addEventListener('click', function()
 {
-    document.querySelector('.modal-overlay').classList.remove('active')
+    modalOverlay.classList.remove('active')
+})
+
+// Maximizar o Modal
+
+const modal = document.querySelector('.modal')
+
+document.querySelector('.maximize-modal').addEventListener('click', function()
+{   
+    if (modal.classList.contains('maximize'))
+    {
+        modal.classList.remove('maximize')
+    }
+    else
+    {
+        modal.classList.add('maximize')
+    }    
 })

5817c8b27ea4546ef7b8f1e78bd3ce04023dae60
Lucas <abreu.lucass@outlook.com>
2020-01-30 22:23:09 -0300

    challenge 03-2 completed
    finished the second challenge from rocketseat's launchbase bootcamp.

---
+++ 3. Iniciando no Back-end/03-2/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const cards = require('./data')
+
+const server = express()
+
+server.set('view engine','njk')
+
+server.use(express.static('public'))
+
+nunjucks.configure('views',
+{
+    express: server
+})
+
+server.get('/', function(req, res)
+{
+    res.render('courses', { cards })
+})
+
+server.get('/about', function(req, res)
+{
+    res.render('about')
+})
+
+server.use(function(req, res)
+{
+    res.status(404).render("not-found");
+});
+
+server.listen(5000, function()
+{
+    console.log('Server started!')
+})
